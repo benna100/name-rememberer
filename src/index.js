1226fd001806d3580a75ea4220cb6b1045367398
behuFTP <benjamin.dals.hughes@gmail.com>
2022-04-13 15:21:38 +0200

    database support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import "./main.scss";
+
 document.querySelector("button").addEventListener("click", () => {
     checkAndGetData(input.value);
 });
 const loading = document.querySelector("span.loading");
 
 const input = document.querySelector("input");
+
+let selectedNodeId;
 
 const login_url_base = window.location.host.includes("localhost")
     ? "http://localhost:5001/name-rememberer-8ed08/us-central1/app"
@@ -22,28 +26,41 @@ function populateSelectList(nodes) {
     select.innerHTML = optionHtmlString;
 }
 
+let nodes = [];
+let edges = [];
+
 function checkAndGetData(password) {
     fetch(`${login_url_base}/data`, {
         method: "POST",
         body: JSON.stringify({ password }),
+        headers: {
+            "Content-Type": "application/json",
+        },
     })
         .then((resp) => resp.json())
         .then((resp) => {
             if (!resp.successful) {
                 return alert("wrong password");
             }
-            localStorage.password = password;
-            draw(resp.data);
-            populateSelectList(resp.data.nodes);
+            try {
+                localStorage.password = password;
+                console.log(resp);
+                nodes = resp.data.nodes;
+                edges = resp.data.edges;
+                draw();
+                populateSelectList(resp.data.nodes);
+            } catch (error) {
+                console.log(error);
+            }
         })
         .catch(() => {
-            alert("Fetching data failed");
+            alert("Fetching data failed 1");
         });
 }
 
 // Called when the Visualization API is loaded.
-function draw({ edges, nodes }) {
-    nodes = nodes.map(({ image, id, label, color }) => {
+function draw() {
+    const nodesData = nodes.map(({ image, id, label, color }) => {
         const obj = {
             id,
             image,
@@ -56,6 +73,9 @@ function draw({ edges, nodes }) {
 
         return obj;
     });
+
+    nodes = new vis.DataSet(nodesData);
+    edges = new vis.DataSet(edges);
 
     // create people.
     // value corresponds with the age of the person
@@ -95,6 +115,59 @@ function draw({ edges, nodes }) {
     network.on("afterDrawing", () => {
         loading.style.display = "none";
     });
+
+    const popup = document.querySelector(".popup");
+    const closePopupElement = popup.querySelector(".close");
+    const popupButton = popup.querySelector("button.update-node");
+
+    const popUpLabelElement = document.querySelector(".popup .label");
+    const popUpImageElement = document.querySelector(".popup .image");
+    const popUpColorElement = document.querySelector(".popup .color");
+
+    popupButton.addEventListener("click", () => {
+        fetch(`${login_url_base}/node/${selectedNodeId}`, {
+            method: "PUT",
+            body: JSON.stringify({
+                label: popUpLabelElement.value,
+                image: popUpImageElement.value,
+                color: popUpColorElement.value,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((resp) => resp.json())
+            .then((resp) => {
+                popup.classList.remove("visible");
+            });
+    });
+
+    closePopupElement.addEventListener("click", () =>
+        popup.classList.remove("visible")
+    );
+    network.on("click", function (params) {
+        const clickedNodeId = params.nodes[0];
+        const clickedEdgeId = params.edges[0];
+        if (clickedNodeId != undefined) {
+            selectedNodeId = clickedNodeId;
+            popup.classList.add("visible");
+
+            fetch(`${login_url_base}/node/${clickedNodeId}`)
+                .then((resp) => resp.json())
+                .then((resp) => {
+                    popUpLabelElement.value = resp.label;
+                    popUpImageElement.value = resp.image;
+                    popUpColorElement.value = resp.color;
+                })
+                .catch(() => {
+                    alert("Fetching data failed");
+                });
+        }
+
+        if (clickedEdgeId != undefined) {
+            console.log(clickedEdgeId);
+        }
+    });
 }
 
 document
@@ -121,10 +194,19 @@ document
                 connectionLabel,
                 password: localStorage.password,
             }),
+            headers: {
+                "Content-Type": "application/json",
+            },
         })
             .then((resp) => resp.json())
             .then((resp) => {
-                console.log(resp);
+                console.log("hereeee");
+                let { id, label, color, image } = resp.nodeCreated;
+                nodes.add({ id, label, color, image });
+                console.log("node", { id, label, color, image });
+
+                let { from, to } = resp.edgeCreated;
+                edges.add({ from, to, label: resp.edgeCreated.label });
             })
             .catch(() => {
                 alert("Fetching data failed");
